34d29264c40a0397370778c93b50e05c91a93559
sachin saluja <sachin.saluja132@gmail.com>
2012-05-28 20:21:21 +0530

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   root              to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
