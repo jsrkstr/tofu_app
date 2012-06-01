635bfd27a795deeb54a912a44e5b4b3b5321b295
sachin saluja <sachin.saluja132@gmail.com>
2012-06-02 00:58:15 +0530

    ssl false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ SampleApp::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, 
   # and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = false
 
   # Code is not reloaded between requests
   config.cache_classes = true
